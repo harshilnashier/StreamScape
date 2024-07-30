d47b8901469a10974c13fbc65b2212d0f432eef2
harshilnashier <93786527+harshilnashier@users.noreply.github.com>
2024-07-30 22:20:05 +0530

    Update video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,5 +1,5 @@
 import { ApiError } from "../utils/ApiError.js"
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { Video } from "../models/video.models.js";
